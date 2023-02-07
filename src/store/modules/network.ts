7684c859d525843df62cda1cf6b5bcdd2f591831
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2023-02-08 00:01:15 +0100

    init

---
+++ src/store/modules/network.ts
@@ -0,0 +1,78 @@
+import { useEthers } from 'vue-dapp';
+import tokens from "../../abi/tokens.json";
+
+const { chainId } = useEthers();
+
+export default {
+  namespaced: true,
+  
+  state: () => ({
+    networkCurrency: "MATIC", // @todo
+    networkName: "Unsupported Network",
+    supportedNetworks: {
+      80001: "Polygon Testnet" // @todo
+    }
+  }),
+
+  getters: { 
+    getBlockExplorerBaseUrl() {
+      return "https://mumbai.polygonscan.com"; // @todo
+    },
+    
+    getChainId() {
+      return chainId.value;
+    },
+
+    getNetworkCurrency(state) {
+      return state.networkCurrency;
+    },
+
+    getNetworkName(state) {
+      const supportedIds = Object.keys(state.supportedNetworks);
+
+      if (supportedIds && supportedIds.includes(String(chainId.value))) {
+        return state.networkName;
+      }
+
+      return "Unsupported Network";
+    },
+
+    getSupportedNetworks(state) {
+      return state.supportedNetworks;
+    },
+
+    getSupportedNetworkIds(state) {
+      return Object.keys(state.supportedNetworks);
+    },
+
+    getSupportedNetworkNames(state) {
+      return Object.values(state.supportedNetworks);
+    },
+
+    getTokens(state) {
+      return tokens[String(chainId.value)]
+    },
+
+    isNetworkSupported(state) {
+      const supportedIds = Object.keys(state.supportedNetworks);
+
+      if (supportedIds && supportedIds.includes(String(chainId.value))) {
+        return true;
+      }
+
+      return false;
+    }
+  },
+
+  mutations: { 
+    setNetworkData(state) {
+      state.networkName = "Polygon Testnet"; // @todo
+      state.networkCurrency = "MATIC"; // @todo
+    }
+  },
+
+  actions: { 
+    
+  }
+
+};
